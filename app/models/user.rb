7328467f508ab0ac7795e6ba274d65db35abd379
Kai Prout <kaiprt@gmail.com>
2014-10-22 21:08:49 -0400

    Added associations to user table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :experiences
+  has_many :events, through: :experiences
+  has_many :checklists, through: :events
+  has_many :items, through: :checklists # **Want User.first.items** 
+
 end
