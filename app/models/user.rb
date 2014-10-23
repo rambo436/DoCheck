74cc74bc67011e33cfd31f34d1bb0511a2efa549
Kai Prout <kaiprt@gmail.com>
2014-10-22 21:14:36 -0400

    Adding associations to (M) user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_many :experiences
   has_many :events, through: :experiences
   has_many :checklists, through: :events
-  has_many :items, through: :checklists # **Want User.first.items** 
+  has_many :items, through: :checklists # **Want User.first.items**
 
 end
