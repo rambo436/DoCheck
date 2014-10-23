d8abd9f3b85c7a8b1206835b0048e4f64a1d4e41
Kai Prout <kaiprt@gmail.com>
2014-10-23 18:56:21 -0400

    Adding validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   has_many :events, through: :experiences
   has_many :checklists, through: :events
   has_many :items, through: :checklists # **Want User.first.items**
+
+  validates_presence_of :first_name, :last_name, :email, :password, :phone
+  validates_uniqueness_of  :email, :phone
+  validate :first_name, :last_name, length: { minimum: 3}
 end
