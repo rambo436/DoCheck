8c6ded4feff8810e6377b7d8f348c0c934b806e2
Kai Prout <kaiprt@gmail.com>
2014-10-24 17:00:50 -0400

    Adding tests for associations.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,10 +7,15 @@ RSpec.describe User, :type => :model do
     user = User.new(first_name: "Kai", last_name:"Prout", email: "kai@example.com")
     expect(user).to be_valid
   end
-  it { should validate_presence_of :first_name}
-  it { should validate_presence_of :last_name}
-  it { should validate_presence_of :email}
-  
+  it { should validate_presence_of :first_name }
+  it { should validate_presence_of :last_name }
+  it { should validate_presence_of :email }
+
+  it { should have_many(:experiences) }
+  it { should have_many(:events).through(:experiences) }
+  it { should have_many(:checklists).through(:events) }
+  it { should have_many(:items).through(:checklists) }
+
   it "is invalid with a duplicate email address" do
     User.create(first_name: "John", last_name:"Foo",
     email: 'foo@foo.com')
