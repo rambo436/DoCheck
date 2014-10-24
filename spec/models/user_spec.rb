a45b79d60cf5d4c4f36db3cd65b3e39e6fe180af
Kai Prout <kaiprt@gmail.com>
2014-10-24 17:06:33 -0400

    Cleaning up rspec file.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,18 +3,22 @@ require 'spec_helper'
 
 RSpec.describe User, :type => :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  it "is valid with a firstname, lastname, and email" do
-    user = User.new(first_name: "Kai", last_name:"Prout", email: "kai@example.com")
-    expect(user).to be_valid
+  describe "Associations" do
+    it { should have_many(:experiences) }
+    it { should have_many(:events).through(:experiences) }
+    it { should have_many(:checklists).through(:events) }
+    it { should have_many(:items).through(:checklists) }
   end
-  it { should validate_presence_of :first_name }
-  it { should validate_presence_of :last_name }
-  it { should validate_presence_of :email }
 
-  it { should have_many(:experiences) }
-  it { should have_many(:events).through(:experiences) }
-  it { should have_many(:checklists).through(:events) }
-  it { should have_many(:items).through(:checklists) }
+  describe "validations" do
+    it "is valid with a firstname, lastname, and email" do
+      user = User.new(first_name: "Kai", last_name:"Prout", email: "kai@example.com")
+      expect(user).to be_valid
+    end
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :email }
+  end
 
   it "is invalid with a duplicate email address" do
     User.create(first_name: "John", last_name:"Foo",
